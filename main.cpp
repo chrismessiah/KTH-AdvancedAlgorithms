7f43f11806f9d3fb0a0c10d83f3ae91b62838a80
Christian Abdelmassih <chrabd@kth.se>
2016-10-31 14:50:31 +0100

    Add input reader

--- main.cpp
+++ main.cpp
@@ -8,16 +8,61 @@
 #include <math.h>
 #include <vector>
 #include <iostream>
+#include <sstream>
 using namespace std;
-// Imports
 
-// Declarations
-float dist(int a, int b)
-// Declarations
+// global variables
+int n;
+vector<vector<float> > coordinates;
+
+// function declaractions
+void getInput();
+void getFirstLine();
+void resizeCoordinateVector();
+float dist(int a, int b);
 
 int main() {
-	cout << "Hello!\n";
+	getFirstLine();
+	resizeCoordinateVector();
+	getInput();
+
+	// testing for globality
+	for (int i = 0; i < ::n; ++i) {
+		cout << ::coordinates[i][0];
+		cout << ::coordinates[i][1];
+	}
+	cout << ::n;
+
 	return 0;
+}
+
+void getFirstLine() {
+	string input = 	"";
+	
+	getline(cin, input);
+	stringstream myStream(input);
+	myStream >> ::n;
+}
+
+void resizeCoordinateVector() {
+	::coordinates.resize(::n);
+	for (int i = 0; i < ::n; ++i) {
+		::coordinates[i].resize(2);
+	}
+} 
+
+void getInput() {
+	string input = 	"";
+
+	for (int i = 0	; i < ::n; ++i) {
+		getline(cin, input);
+    	istringstream buf(input);
+    	istream_iterator<string> beg(buf), end;
+    	vector<string> tokens(beg, end);
+
+		::coordinates[i][0] = atof(tokens[0].c_str());
+		::coordinates[i][1] = atof(tokens[1].c_str());
+	}
 }
 
 vector<int> GreedyTour(int n) {
