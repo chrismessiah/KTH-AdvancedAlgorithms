c137c3af3342e614ff79a35609c623ac8541c713
Marcus <marcus@wallberg.nu>
2016-10-31 11:17:12 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// Travelling Salesperson 2D
+// https://kth.kattis.com/problems/tsp
+
+// Imports
+#include <math.h>
+#include <vector>
+#include <iostream>
+using namespace std;
+// Imports
+
+int main() {
+    cout << "Hello!\n";
+    return 0;
+}
