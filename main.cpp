102c9ab9b71f94e542c50bbc09070130a1efcef4
Christian Abdelmassih <chrabd@kth.se>
2016-10-31 11:46:06 +0100

    Add GreedyTour function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 // Travelling Salesperson 2D
 // https://kth.kattis.com/problems/tsp
 
+// compile with: g++ filename.cpp
+// Run with: ./a.out
+
 // Imports
 #include <math.h>
 #include <vector>
@@ -9,6 +12,27 @@ using namespace std;
 // Imports
 
 int main() {
-    cout << "Hello!\n";
-    return 0;
+	cout << "Hello!\n";
+	return 0;
+}
+
+vector<int> GreedyTour(int n) {
+	// initalize some variables
+	vector<int> tour(n);
+	vector<bool> used(n);
+	used[0] = true;
+	int best = -1;
+
+	// run the actual algorithm
+	for (int i = 1; i < n; ++i) {
+		best = -1;
+		for (int j = 0; j < n; ++j) {
+			if (!used[j] && (best == -1 || dist(tour[i-1],j) < dist(tour[i-1],best) ) ) {
+				best = j;
+			}
+		}
+		tour[i] = best;
+		used[best] = true;
+	}
+	return tour;
 }
