984fd5a627f8a036bde8f558184fcc1207af3813
Christian Abdelmassih <chrabd@kth.se>
2016-10-31 18:30:10 +0100

    Load test-case automagically

--- main.cpp
+++ main.cpp
@@ -32,6 +32,7 @@ vector<int> greedyTour();
 void printTour(vector<int> output);
 float calculateTourCost(vector<int> tour);
 vector<int> getRandomTour();
+void loadTestValues();
 
 
 
@@ -62,6 +63,11 @@ void printTour(vector<int> output) {
 }
 
 void getInput() {
+	if (!::kattis) {
+		loadTestValues();
+		return;
+	}
+
 	cin >> ::n;
 	
 	// set size of vector
@@ -79,10 +85,45 @@ void getInput() {
 
 
 
-// return random int between 0 and upperBound
-int getRandomInt(int upperBound) {
-	srand (time(NULL));
-	return rand() % upperBound;
+
+
+void loadTestValues() {
+	::n = 10;
+
+	::coordinates.resize(::n);
+	for (int i = 0; i < ::n; ++i) {
+		::coordinates[i].resize(2);
+	}
+
+	::coordinates[0][0] = 95.0129;
+	::coordinates[0][1] = 61.5432;
+
+	::coordinates[1][0] = 23.1139;
+	::coordinates[1][1] = 79.1937;
+
+	::coordinates[2][0] = 60.6843;
+	::coordinates[2][1] = 92.1813;
+
+	::coordinates[3][0] = 48.5982;
+ 	::coordinates[3][1] = 73.8207;
+
+	::coordinates[4][0] = 89.1299;
+ 	::coordinates[4][1] = 17.6266;
+
+	::coordinates[5][0] = 76.2097;
+ 	::coordinates[5][1] = 40.5706;
+
+	::coordinates[6][0] = 45.6468;
+ 	::coordinates[6][1] = 93.5470;
+
+	::coordinates[7][0] = 1.8504;
+ 	::coordinates[7][1] = 91.6904;
+
+	::coordinates[8][0] = 82.1407;
+ 	::coordinates[8][1] = 41.0270;
+
+	::coordinates[9][0] = 44.4703;
+ 	::coordinates[9][1] = 89.3650;
 }
 
 // REMEMBER 
