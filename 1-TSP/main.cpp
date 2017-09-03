e3feb9dfc37e6042fd690d5a0fd19933ea157731
Christian Abdelmassih <chrabd@kth.se>
2017-09-03 12:23:04 +0200

    Add concept-matrix as a 1D array

--- 1-TSP/main.cpp
+++ 1-TSP/main.cpp
@@ -10,30 +10,40 @@
 
 #include <iostream>
 #include <vector>
+#include <math.h>
 
 using namespace std;
 
-#include "distance.hpp"
-#include "inputs.hpp"
+#include "helpers.hpp"
+#include "test_input.hpp"
+
+int kattis;
+short inputLength;
 
 int main() {
-  bool kattis = false;
-  short inputLength;
-
+  kattis = false;
   if (kattis) { cin >> inputLength; }
   else {inputLength = get_test_input_length();}
 
+  // get inputs, store in vectors
   vector<double> x(inputLength);
   vector<double> y(inputLength);
-
   if (kattis) {for (short i = 0; i < inputLength; i++) { cin >> x[i] >> y[i];}}
   else {get_test_input(&x, &y);}
 
-  // change this to store the distance instead in some type of datastructure
-  long dist;
-  for (short i = 1; i < inputLength; i++) {
-    dist = distance( &(x[i]), &(x[i-1]), &(y[i]), &(x[i-1]) );
-    cout << dist << "\n";
+  // convert inputs to distance matrix
+  long dMatrix[inputLength*inputLength];
+
+  // set all values to 0, for testing purposes
+  if (!kattis) {for (short i = 0; i < inputLength; i++) {for (short j = 0; j < inputLength; j++) {dMatrix[inputLength*i + j] = 0;}}}
+
+  // insert data values
+  for (short i = 0; i < inputLength; i++) {
+    for (short j = i+1; j < inputLength; j++) {
+      dMatrix[inputLength*i + j] = dMatrix[inputLength*j + i] = round(sqrt( ((x[i]-x[j])*(x[i]-x[j])) + ((y[i]-y[j])*(y[i]-y[j])) ));
+    }
   }
+
+  print_matrix(inputLength, dMatrix);
   return 0;
 }
